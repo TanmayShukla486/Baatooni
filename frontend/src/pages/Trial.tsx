02fd061a13e68753cde9dcb886a34bd2bdb82ef0
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-07-19 19:32:48 +0530

    trial file to test designs

--- frontend/src/pages/Trial.tsx
+++ frontend/src/pages/Trial.tsx
@@ -1,5 +1,14 @@
+import BackgroundCard from '../components/BackgroundCard';
+import Navbar from '../components/Navbar';
+
 const Trial = () => {
-  return <div></div>;
+  return (
+    <div className="w-full h-screen overflow-y-auto py-12 flex justify-center items-center scrollbar-hide">
+      <BackgroundCard className="w-ninety min-h-screen h-fit overflow-y-auto mt-14">
+        <Navbar />
+      </BackgroundCard>
+    </div>
+  );
 };
 
 export default Trial;
