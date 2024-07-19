d694b72014f4ceafab033ca4e222f4131d61b083
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-07-19 19:08:24 +0530

    added the typing animation code as well as other custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,9 +8,33 @@ export default {
   ],
   theme: {
     extend: {
+      keyframes: {
+        typing: {
+          '0%': {
+            width: '0%',
+            visibility: 'hidden',
+          },
+          '100%': {
+            width: '100%',
+          },
+        },
+        blink: {
+          '50%': {
+            borderColor: 'transparent',
+          },
+          '100%': {
+            borderColor: 'white',
+          },
+        },
+      },
+      animation: {
+        typing: 'typing 1.5s steps(20) alternate, blink .7s infinite',
+      },
       colors: {
         start: '#021861',
         end: '#521638',
+        textStart: '#8c0f56',
+        textEnd: '#1447f0',
       },
       size: {
         ninety: '90%',
